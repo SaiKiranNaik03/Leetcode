e5c6185ea2a9833cf5480325ab34a2f1006b2e55
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2024-06-03 21:13:51 +0530

    Time: 49 ms (43.08%), Space: 47.5 MB (34.30%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int goodArray(int nums[],int k){
+        int n = nums.length;
+        int left=0,right=0,count=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while(right < n){
+            if(map.containsKey(nums[right])){
+                map.put(nums[right],map.get(nums[right])+1);
+            }
+            else{
+                map.put(nums[right],1);
+            }
+            while(map.size() > k){
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            count += (right-left+1);
+            right++;
+        }
+        return count;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return goodArray(nums,k) - goodArray(nums,k-1);
+    }
+}
