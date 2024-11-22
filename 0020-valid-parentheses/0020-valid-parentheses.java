f64f200fc07818a22631344185ecd1f96ef027e9
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2024-11-22 21:10:53 +0530

    Time: 2 ms (97.31%), Space: 41.4 MB (73.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,41 @@
 class Solution {
-    public boolean isValid(String s) {
+
+    // public boolean isValid(String s) {
+    //     Stack<Character> stk = new Stack<>();
+    //     for(char ch : s.toCharArray()){
+    //         if(ch == '(' || ch == '{' || ch == '['){
+    //             stk.push(ch);
+    //         }
+    //         else{
+    //             if(stk.isEmpty()){
+    //                 return false;
+    //             }
+    //             char out = stk.pop();
+    //             if((ch == ')' && out == '(') || (ch == ']' && out == '[') || (ch == '}' && out == '{')){
+    //                 continue;
+    //             }
+    //             else{
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return stk.isEmpty();
+    // }
+
+    public boolean isValid(String s){
         Stack<Character> stk = new Stack<>();
         for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '{' || ch == '['){
-                stk.push(ch);
+            if(ch == '{'){
+                stk.push('}');
             }
-            else{
-                if(stk.isEmpty()){
-                    return false;
-                }
-                char out = stk.pop();
-                if((ch == ')' && out == '(') || (ch == ']' && out == '[') || (ch == '}' && out == '{')){
-                    continue;
-                }
-                else{
-                    return false;
-                }
+            else if(ch == '['){
+                stk.push(']');
+            }
+            else if(ch == '('){
+                stk.push(')');
+            }
+            else if(stk.isEmpty() || stk.pop() != ch){
+                return false;
             }
         }
         return stk.isEmpty();
