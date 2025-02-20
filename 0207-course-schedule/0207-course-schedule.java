d81c19a3f208e685e3b864c8f485194f47b89d7f
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-02-20 19:46:44 +0530

    Time: 7 ms (51.03%), Space: 45.5 MB (56.83%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,51 @@
+class Solution {
+    static void dfs(int node, int vist[], Stack<Integer> stk, ArrayList<ArrayList<Integer>> adj) {
+        vist[node] = 1;
+        for (Integer n : adj.get(node)) {
+            if (vist[n] == 0) {
+                dfs(n, vist, stk, adj);
+            }
+        }
+        stk.push(node);
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Construct adjacency list
+        for (int[] pre : prerequisites) {
+            adj.get(pre[1]).add(pre[0]);
+        }
+
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            for (int neighbor : adj.get(i)) {
+                indegree[neighbor]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int count = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            count++;
+            for (int neighbor : adj.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        return count == numCourses; // Return true if we can take all courses (no cycle)
+    }
+}
