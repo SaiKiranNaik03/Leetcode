aebe5ff72fd18b3b14a4d0dc4cee295f2783ffde
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-05-12 11:40:44 +0530

    Time: 2 ms (99.33%), Space: 50.2 MB (8.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,38 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int c1=0,c2=0;
-        int ele1,ele2;
-        ele1 = ele2 = Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(c1 == 0 && nums[i] != ele2){
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+
+        for(int ele : nums){
+            if(c1 == 0 && ele2 != ele){
+                c1=1;
+                ele1=ele;
+            }else if(c2==0 && ele1!=ele){
+                c2=1;
+                ele2=ele;
+            }else if(ele == ele1){
                 c1++;
-                ele1 = nums[i];
-            }
-            else if(c2 == 0 && nums[i] != ele1){
+            }else if(ele == ele2){
                 c2++;
-                ele2 = nums[i];
-            }
-            else if(nums[i] == ele1){
-                c1++;
-            }
-            else if(nums[i] == ele2){
-                c2++;
-            }
-            else{
+            }else{
                 c1--;
                 c2--;
             }
         }
-        List<Integer> ls = new ArrayList<>();
-        c1 = 0; c2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == ele1) c1++;
-            if (nums[i] == ele2) c2++;
-        }
 
-        int mini = (nums.length / 3) + 1;
-        if (c1 >= mini) ls.add(ele1);
-        if (c2 >= mini) ls.add(ele2);
-        return ls;
+        c1=0;c2=0;
+        for(int ele : nums){
+            if(ele == ele1) c1++;
+            if(ele == ele2) c2++;
+        }
+        ArrayList<Integer> res =  new ArrayList<>();
+        int mini = nums.length/3 + 1;
+        if(c1 >= mini) res.add(ele1);
+        if(c2 >= mini) res.add(ele2);
+
+        return res;
     }
 }
