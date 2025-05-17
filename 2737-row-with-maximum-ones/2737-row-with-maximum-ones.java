c5fe30910916e830b4e061f6f3a473dd177a1c5d
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-05-17 10:27:22 +0530

    Time: 1 ms (100%), Space: 46.3 MB (96.31%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -1,31 +1,18 @@
 class Solution {
-    static int lowerB(int arr[]){
-        Arrays.sort(arr);
-        int low=0;
-        int high = arr.length-1;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(arr[mid] == 1){
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-        }
-        return arr.length - low;
-    }
     public int[] rowAndMaximumOnes(int[][] mat) {
-        int n =mat.length;
-        int m = mat[0].length;
-        int res[] = new int[2];
-        int maxi = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            int ones = lowerB(mat[i]);
-            if(ones > maxi){
-                maxi = ones;
-                res[0]=i;
-                res[1]=maxi;
+        int max = 0;
+        int[] sol = new int[2];
+        for (int i = 0; i < mat.length; i++) {
+            int c = 0;
+            for (int j : mat[i]) {
+                c += j;
+            }
+            if (c > max) {
+                sol[0] = i;
+                sol[1] = c;
+                max = c;
             }
         }
-        return res;
+        return sol;
     }
 }
