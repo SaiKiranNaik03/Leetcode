01a839e5fa82f9d3fafd56b9d9ee2bdba43b5e87
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-07-16 13:38:01 +0530

    Time: 18 ms (62.19%), Space: 44 MB (48.32%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); 
+
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i != 0 && nums[i] == nums[i - 1]) continue; 
             for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int k = j + 1, l = n - 1;
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
+
+                int k = j + 1;
+                int l = n - 1;
+
                 while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l]; 
+
                     if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
+
                         while (k < l && nums[k] == nums[k - 1]) k++;
+
                         while (k < l && nums[l] == nums[l + 1]) l--;
+
                     } else if (sum > target) {
                         l--;
                     } else {
@@ -24,6 +32,6 @@ class Solution {
                 }
             }
         }
-        return result;
+        return res;
     }
 }
