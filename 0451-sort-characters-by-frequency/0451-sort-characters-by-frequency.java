7073d7a35176aa09e2c0e5ff0e11df0ce05c467f
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-07-18 16:12:40 +0530

    Time: 16 ms (37.79%), Space: 46.2 MB (14.23%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,39 +1,24 @@
-class Pair {
-    char first;
-    int second;
-
-    Pair(char first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        Map<Character, Integer> hm = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.second - a.second);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
-        }
-
+        
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> b.getValue() - a.getValue()
+        );
+        
+        pq.addAll(hm.entrySet());
+        
+        StringBuilder result = new StringBuilder();
         while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            char first = p.first;
-            int second = p.second;
-            while (second > 0) {
-                sb.append(first);
-                second--;
-            }
+            Map.Entry<Character, Integer> entry = pq.poll();
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
         }
-        return sb.toString();
+        
+        return result.toString();
     }
 }
+
