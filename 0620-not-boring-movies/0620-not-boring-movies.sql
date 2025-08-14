b85f7c917840185912d4469b6f6942260467e18b
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-08-14 22:19:54 +0530

    Time: 407 ms (7.45%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,2 @@
--- Write your PostgreSQL query statement below
-SELECT * FROM Cinema
-WHERE id % 2 = 1 AND description != 'boring'
-ORDER BY rating DESC;
+# Write your MySQL query statement below
+select * from cinema where Mod(id,2) = 1 and Not(description = 'boring') order by rating desc;
