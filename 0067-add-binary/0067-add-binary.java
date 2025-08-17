7f5ef8cbc14ea191844fd79105db3e6cc2508e64
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-08-17 22:53:45 +0530

    Time: 0 ms (100%), Space: 42 MB (91.86%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,21 +1,15 @@
-class Solution 
-{
-  public String addBinary(String a, String b) 
-  {
-    StringBuilder sb = new StringBuilder();
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-
-    while (i >= 0 || j >= 0 || carry == 1) 
-    {
-      if(i >= 0)
-        carry += a.charAt(i--) - '0';
-      if(j >= 0)
-        carry += b.charAt(j--) - '0';
-      sb.append(carry % 2);
-      carry /= 2;
+class Solution {
+    public String addBinary(String a, String b) {
+     if(a.length() < b.length()) return addBinary(b,a);
+        int i=a.length()-1, j=b.length()-1, k=a.length(), carry=0;
+        char[] res=new char[a.length()+1];
+        while(k>0 || carry==1){
+            int char1= i>=0 ? a.charAt(i--) - '0' :0;
+            int char2= j>=0 ? b.charAt(j--) - '0' :0;
+            int sum=carry+char1+char2;
+            res[k--]=(char)((sum & '1')+'0');
+            carry=sum>>1;
+        }
+        return new String(res, k+1, res.length-k-1);
     }
-    return sb.reverse().toString();
-  }
 }
