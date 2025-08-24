7c6a34836715d899e175fc5ed669babec89a7b73
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-08-24 21:29:23 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.8%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,15 @@
 class Solution {
-    // Tabulation
+    // Tabulation - optimal
     public int fib(int n) {
         if(n == 0) return 0;
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-
+        int a = 0;
+        int b = 1;
+        int c = a + b;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a + b;
+            a=b;
+            b=c;
         }
-        return dp[n];
+        return c;
     }
 }
