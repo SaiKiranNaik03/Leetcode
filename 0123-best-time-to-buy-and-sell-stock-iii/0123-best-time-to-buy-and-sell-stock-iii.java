279a8884172709a24dce74eb27dfbb1037c2afd2
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-08-28 12:18:13 +0530

    Time: 63 ms (39.24%), Space: 64.2 MB (37.86%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -11,14 +11,46 @@ class Solution {
         }
         return dp[idx][buy][maxi] = profit;
     }
+    // public int maxProfit(int[] prices) {
+    //     int n = prices.length;
+    //     int maxi = 2;
+    //     int dp[][][] = new int[n][2][maxi+1];
+    //     for (int i = 0; i < n; i++)
+    //         for (int j = 0; j < 2; j++)
+    //             Arrays.fill(dp[i][j], -1);
+        
+    //     return fun(0,1,prices,n,maxi,dp);
+    // }
+
+    // Tabulation
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int maxi = 2;
-        int dp[][][] = new int[n+1][2][maxi+1];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < 2; j++)
-                Arrays.fill(dp[i][j], -1);
-        
-        return fun(0,1,prices,n,maxi,dp);
+        int cap = 2;
+        int dp[][][] = new int[n+1][2][cap+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j][0] = 0;
+            }
+        }
+        for(int i=0;i<2;i++){
+            for(int j=0;j<=cap;j++){
+                dp[n][i][j] = 0;
+            }
+        }
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<2;buy++){
+                for(int maxi=1;maxi<=2;maxi++){
+                    int profit = 0;
+                    if(buy == 1){
+                        profit = Math.max(-prices[idx] + dp[idx+1][0][maxi] , 0 + dp[idx+1][1][maxi]);
+                    }
+                    else{
+                        profit = Math.max(prices[idx] + dp[idx+1][1][maxi-1] , 0 + dp[idx+1][0][maxi]);
+                    }
+                    dp[idx][buy][maxi] = profit;
+                }
+            }
+        }
+        return dp[0][1][cap];
     }
 }
