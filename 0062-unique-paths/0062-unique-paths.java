99f8c91b2b87ec61fafd9aaca48d4a91f072ad82
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-08-31 13:38:13 +0530

    Time: 0 ms (100%), Space: 40.6 MB (35.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,16 @@
 class Solution {
+    int fun(int i, int j, int dp[][]){
+        if(i == 0 && j == 0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = fun(i-1,j,dp);
+        int left = fun(i,j-1,dp);
+
+        return dp[i][j] = up+left;
+    }
     public int uniquePaths(int m, int n) {
-        int N = n+m-2;
-        int R = m-1;
-        double res = 1;
-        for(int i=1;i<=R;i++){
-            res = res*(N-R+i)/i;
-        }
-        return (int)res;
+        int dp[][] = new int[n][m];
+        for(int ro[] : dp) Arrays.fill(ro, -1);
+        return fun(n-1,m-1,dp);
     }
 }
