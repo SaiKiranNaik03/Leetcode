bb823099f5f52541a43ce310cdf8bd0ac27035cc
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-09-27 17:45:02 +0530

    Time: 2 ms (99.73%), Space: 50.3 MB (68.24%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int c1=0,c2=0;
-        int ele1 = Integer.MIN_VALUE;
-        int ele2 = Integer.MIN_VALUE;
+        int c1,c2,ele1,ele2;
+        c1=c2=0;
+        ele1=ele2=Integer.MIN_VALUE;
 
         for(int ele : nums){
-            if(c1 == 0 && ele2 != ele){
-                c1=1;
-                ele1=ele;
-            }else if(c2==0 && ele1!=ele){
-                c2=1;
-                ele2=ele;
-            }else if(ele == ele1){
-                c1++;
-            }else if(ele == ele2){
-                c2++;
-            }else{
+            if(c1 == 0 && ele != ele2){
+                c1 = 1;
+                ele1 = ele;
+            }else if(c2 == 0 && ele != ele1){
+                c2 = 1;
+                ele2 = ele;
+            }
+            else if(ele1 == ele) c1++;
+            else if(ele2 == ele) c2++;
+            else{
                 c1--;
                 c2--;
             }
         }
-
-        c1=0;c2=0;
+        List<Integer> res = new ArrayList<>();
+        c1 = c2 = 0;
+        int mini = (nums.length/3)+1;
         for(int ele : nums){
-            if(ele == ele1) c1++;
-            if(ele == ele2) c2++;
+            if(ele1 == ele) c1++;
+            if(ele2 == ele) c2++;
         }
-        ArrayList<Integer> res =  new ArrayList<>();
-        int mini = nums.length/3 + 1;
         if(c1 >= mini) res.add(ele1);
         if(c2 >= mini) res.add(ele2);
-
         return res;
     }
 }
