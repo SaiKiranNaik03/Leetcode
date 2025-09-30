31f688e9e81baf548cc4f1048e1261d09c5d5714
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-09-30 23:08:34 +0530

    Time: 11 ms (76.77%), Space: 47.6 MB (21.52%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,39 +1,33 @@
 class Solution {
-    static int[] findLH(int arr[]){
-        int low=arr[0];
-        int high=0;
-        for(int el : arr){
-            if(el > low){
-                low=el;
-            }
-            high+=el;
-        }
-        return new int[]{low,high};
-    }
-    static int findDays(int arr[], int wt){
-        int total=0,c=0;
+    int findD(int arr[], int cap){
+        int days = 0;
+        int c = 0;
         for(int w : arr){
-            c+=w;
-            if(c > wt){
-                total++;
+            if(c+w <= cap){
+                c+=w;
+            }else{
+                days++;
                 c=w;
-            }   
+            }
         }
-        if(c > 0)total++;
-        return total;
+        if(c > 0) days++;
+        return days;
     }
     public int shipWithinDays(int[] weights, int days) {
-        int res[] = findLH(weights);
-        int low = res[0];
-        int high = res[1];
+        int low = 0;
+        int high = 0;
+        for(int w : weights){
+             high += w;
+             low = Math.max(low,w);
+        }
 
         while(low <= high){
-            int mid =(low+high)/2;
-            int day = findDays(weights, mid);
-            if(day <= days){
-                high = mid-1;
+            int mid = (low + high)/2;
+            int d = findD(weights, mid);
+            if(d <= days){
+                high = mid  - 1;
             }else{
-                low = mid+1;
+                low = mid +1;
             }
         }
         return low;
