47a563d35590c920f2869ea42d7d42ab0844fd21
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-09-30 12:13:59 +0530

    Time: 0 ms (100%), Space: 46.1 MB (49.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,43 @@
 class Solution {
-    static int first(int arr[], int x){
+    int first(int arr[], int tar){
         int n = arr.length;
+        int ans = -1;
         int low = 0;
         int high = n-1;
-        int ans = -1;
+
         while(low <= high){
-            int mid = (low+high)/2;
-            if(arr[mid] == x){
+            int mid = (low + high)/2;
+            if(arr[mid] == tar){
                 ans = mid;
-                high = mid-1;
-            }else if(arr[mid] > x){
-                high = mid-1;
+                high = mid -1;
+            }else if(arr[mid] < tar){
+                low = mid + 1;
             }else{
-                low = mid+1;
+                high = mid -1;
             }
         }
         return ans;
     }
-    static int last(int arr[], int x){
+    int last(int arr[], int tar){
         int n = arr.length;
+        int ans = -1;
         int low = 0;
         int high = n-1;
-        int ans = -1;
+
         while(low <= high){
             int mid = (low + high)/2;
-            if(arr[mid] == x){
+            if(arr[mid] == tar){
                 ans = mid;
-                low = mid+1;
-            }else if(arr[mid] > x){
-                high = mid-1;
+                low = mid + 1;
+            }else if(arr[mid] < tar){
+                low = mid +1;
             }else{
-                low = mid+1;
+                high = mid -1;
             }
         }
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        return new int[] {first(nums, target), last(nums, target)};
+        return new int[]{first(nums,target), last(nums, target)};
     }
 }
