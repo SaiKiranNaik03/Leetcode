e557ff3d4515a6f72839b25ebe4d941ddc67a12d
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-10-01 18:10:28 +0530

    Time: 3 ms (49.51%), Space: 47.9 MB (48.5%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,37 +1,37 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        // int l,r;
-        // l=r=0;
-        // int c=0;
-        // int sum=0;
-        // int n = nums.length;
-
-        // while(r < n){
-        //     sum += nums[r];
-
-        //     while(sum > goal){
-        //         sum -= nums[l];
-        //         l++;
-        //     }
-        //     if(sum <= goal) c = r-l+1;
-
-        //     r++;
-        // }
-        // return c;
-        int c,sum;
-        sum = c = 0;
+    int getCount(int nums[], int tar){
+        int l,r,sum,c;
+        l=r=sum=c=0;
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
+        while(r < n){
+            sum += nums[r];
 
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-
-            int rem = sum - goal;
-            c += map.getOrDefault(rem,0);
-
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            while(l < n && sum > tar){
+                sum -= nums[l];
+                l++;
+            }
+            if(sum <= tar) c+= r-l+1;
+            r++;
         }
         return c;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        // int c,sum;
+        // sum = c = 0;
+        // int n = nums.length;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(0,1);
+
+        // for(int i=0;i<n;i++){
+        //     sum += nums[i];
+
+        //     int rem = sum - goal;
+        //     c += map.getOrDefault(rem,0);
+
+        //     map.put(sum,map.getOrDefault(sum,0)+1);
+        // }
+        // return c;
+        if(goal-1 < 0) return getCount(nums,goal);
+        return getCount(nums, goal) - getCount(nums,goal-1);
     }
 }
