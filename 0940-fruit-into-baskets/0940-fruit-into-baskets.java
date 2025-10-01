e5b03b64c201b1db048b753d95dc1c87a992d486
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-10-01 17:19:13 +0530

    Time: 51 ms (37.24%), Space: 55.2 MB (61.79%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -6,14 +6,14 @@ class Solution {
         for(int r=0;r<fruits.length;r++){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
 
-            while(map.size() > 2){
+            if(map.size() > 2){
                 map.put(fruits[l],map.get(fruits[l])-1);
                 if(map.get(fruits[l]) == 0){
                     map.remove(fruits[l]);
                 }
                 l++;
             }
-            maxi = Math.max(maxi,r-l+1);
+            if(map.size() <= 2) maxi = Math.max(maxi,r-l+1);
         }
         return maxi;
     }
