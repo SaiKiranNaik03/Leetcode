bdb44aae222f592eac4fa3154f8d03d4dd293e0e
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2025-12-05 19:08:33 +0530

    Time: 8 ms (87.75%), Space: 49 MB (36.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-
-        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
-        for(int i=0;i<intervals.length;i++){
-            if(res.size() == 0 || intervals[i][0] > res.get(res.size()-1).get(1)){
-                res.add(new ArrayList<>(Arrays.asList(intervals[i][0], intervals[i][1])));
-            }
-            else{
-                res.get(res.size()-1).set(1,Math.max(intervals[i][1],res.get(res.size()-1).get(1)));
-            }
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        ArrayList<int[]> res = new ArrayList<>();
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int n = intervals.length;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0] <= end){
+                end = Math.max(end, intervals[i][1]);
+            }else{
+                res.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }   
         }
-        int row = res.size();
-        int result[][] = new int[row][2];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<2;j++){
-                result[i][j] = res.get(i).get(j);
-            }
+        res.add(new int[]{start,end});
+        int len = res.size();
+        int result[][] = new int[len][2];
+
+        for(int i=0;i<len;i++){
+            result[i][0] = res.get(i)[0];
+            result[i][1] = res.get(i)[1];
         }
         return result;
     }
