fd4569e1cfc3b5de21643a18b03ec0be839e35db
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2026-04-02 14:47:14 +0530

    Time: 2597 ms (5%), Space: 55.2 MB (98.94%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
         int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+
         for(int i=0;i<n;i++){
-            if(i!=0 && nums[i] == nums[i-1]) continue;
+            HashSet<Integer> st = new HashSet<>();
 
-            int j = i+1;
-            int k = n-1;
+            for(int j=i+1;j<n;j++){
+                int rem = -(nums[i] + nums[j]);
 
-            while(j < k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum < 0){
-                    j++;
-                }else if(sum > 0){
-                    k--;
-                }else{
-                    List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
-                    // temp.sort(null);
-                    res.add(temp);
-                    k--;
-                    j++;
-                    while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
+                if(st.contains(rem)){
+                    List<Integer> temp =  Arrays.asList(nums[i],nums[j],rem);
+                    Collections.sort(temp);
+                    set.add(temp);
                 }
+                st.add(nums[j]);
             }
         }
-        
+        List<List<Integer>> res = new ArrayList<>(set);
         return res;
     }
 }
