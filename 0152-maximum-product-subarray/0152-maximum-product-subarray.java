5e5385b1aa8bc58cabdc81aee6fa4a3dde3902f4
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2026-04-02 15:13:07 +0530

    Time: 2 ms (69.02%), Space: 47.4 MB (42.4%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int left=1;
-        int right =1;
         int n = nums.length;
+        int left = 1;
+        int right = 1;
         int maxi = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             left *= nums[i];
             right *= nums[n-i-1];
 
-            maxi = Math.max(maxi, Math.max(left,right));
-
-            if(left == 0) left =1;
+            maxi = Math.max(maxi,Math.max(left,right));
+            if(left == 0) left = 1;
             if(right == 0) right = 1;
-
         }
         return maxi;
     }
