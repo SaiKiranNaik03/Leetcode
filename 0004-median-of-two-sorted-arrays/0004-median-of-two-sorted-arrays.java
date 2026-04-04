84e3b6c38bb4e2904a798172b6c5b5acda084c66
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2026-04-04 10:53:00 +0530

    Time: 11 ms (5.13%), Space: 48.7 MB (90.02%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,43 +2,39 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int i = 0, j = 0, c = 0;
-        int idx2 = (n + m) / 2;
-        int idx1 = idx2 - 1;
-        int ele1 = -1;
-        int ele2 = -1;
-
-        while (i < n && j < m) {
-            if (nums1[i] <= nums2[j]) {
-                if (c == idx1) ele1 = nums1[i];
-                if (c == idx2) ele2 = nums1[i];
-                c++;
+        int idx2 = (n+m)/2;
+        int idx1 = idx2-1;
+        int c = 0;
+        int i,j,ele1,ele2;
+        ele1 = ele2 = -1;
+        i=j=0;
+        while(i < n && j<m){
+            if(nums1[i] <= nums2[j]){
+                if(c == idx1) ele1 = nums1[i];
+                if(c == idx2) ele2 = nums1[i];
                 i++;
-            } else {
-                if (c == idx1) ele1 = nums2[j];
-                if (c == idx2) ele2 = nums2[j];
                 c++;
+            }else{
+                if(c == idx1) ele1 = nums2[j];
+                if(c == idx2) ele2 = nums2[j];
                 j++;
+                c++;
             }
         }
-
-        while (i < n) {
-            if (c == idx1) ele1 = nums1[i];
-            if (c == idx2) ele2 = nums1[i];
-            c++;
-            i++;
+        while(i < n){
+            if(c == idx1) ele1 = nums1[i];
+            if(c == idx2) ele2 = nums1[i];
+            i++;c++;
         }
-
-        while (j < m) {
-            if (c == idx1) ele1 = nums2[j];
-            if (c == idx2) ele2 = nums2[j];
-            c++;
-            j++;
+        while(j < m){
+            if(c == idx1) ele1 = nums2[j];
+            if(c == idx2) ele2 = nums2[j];
+            j++;c++;
         }
-
-        if ((n + m) % 2 == 0) {
-            return (ele1 + ele2) / 2.0;
+        System.out.println(ele1+" "+ele2);
+        if((n+m)%2 == 0){
+            return (double)(ele1+ele2)/2.0;
         }
-        return ele2;
+        return (double)ele2;
     }
 }
