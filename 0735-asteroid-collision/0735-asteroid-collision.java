8f24f7cacd7c8aabd0371aa7f4814df8674650d9
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2026-04-07 21:57:16 +0530

    Time: 5 ms (65.99%), Space: 47.1 MB (47.9%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,25 +1,17 @@
 class Solution {
-    public int[] asteroidCollision(int[] arr) {
-        Stack<Integer> stk = new Stack<Integer>();
-        for(int a : arr){
-            if(a > 0){
-                stk.push(a);
-            }
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stk = new Stack<>();
+        for(int a : asteroids){
+            if(a > 0) stk.push(a);
             else{
-                while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < -a){
-                    stk.pop();
-                }
-                if(stk.isEmpty() || stk.peek() < 0){
-                    stk.push(a);
-                }
-                if(stk.peek() == -a){
-                    stk.pop();
-                }
+                while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < -a) stk.pop();
+                if(stk.isEmpty() || stk.peek() < 0) stk.push(a);
+                else if(stk.peek() == -a) stk.pop();
             }
         }
-        int l = stk.size();
-        int res[] = new int[l];
-        int i = l-1;
+        int len = stk.size();
+        int res[] = new int[len];
+        int i=len-1;
         while(!stk.isEmpty()){
             res[i--] = stk.pop();
         }
