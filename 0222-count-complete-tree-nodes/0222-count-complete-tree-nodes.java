cb4396df0f034ce59e81a7dbdacf9ccfca704841
K Sai Kiran Naik <153849300+SaiKiranNaik03@users.noreply.github.com>
2026-05-24 15:05:23 +0530

    Time: 0 ms (100%), Space: 49.2 MB (83.07%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,18 +14,31 @@
  * }
  */
 class Solution {
-    int cnt = 0;
-    void travel(TreeNode node){
-        if(node == null){
-            return ;
-        }else cnt++;
-
-        travel(node.right);
-        travel(node.left);
-
-    }
-    public int countNodes(TreeNode root) {
-        travel(root);
+    int heightL(TreeNode node){
+        int cnt = 0;
+        while(node != null){
+            node = node.left;
+            cnt++;
+        }
         return cnt;
     }
+    int heightR(TreeNode node){
+        int cnt = 0;
+        while(node != null){
+            node = node.right;
+            cnt++;
+        }
+        return cnt;
+    }
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = heightL(root);
+        int right = heightR(root);
+
+        if(left == right) return (1 << left)-1 ;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
 }
